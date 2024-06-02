3144ba224f3caf0f0675b57d8a07385921b2e19f
123Luka123 <abramishvililuka29@gmail.com>
2024-06-02 16:08:17 +0400

    first commit

---
+++ Level3/Classwork/Lesson.py
@@ -0,0 +1,46 @@
+
+
+#task 1 -შექმენით  3 ცვლადი - num1(int), num2(int), num3(float). ( num - number). მოახდინეთ სხვადასხვა მათემატიკური ოპერაციები ამ რიცხვებზე ( 20 მაგალითი)
+
+num1 = 2
+num2 = 14
+num3 = 20
+
+print(num1 + num2) #1
+print(num1 + num3) #2
+print(num2 + num3) #3
+print(num1+num2+num3)#4
+print(num3 - num2) #5
+print(num3 - num1)#6
+print(num2 - num1)#7
+print(num3 - num2 - num1)#8
+print(num1 * num2)#9
+print(num1 * num3)#10
+print(num1 * num2 * num3)#11
+print(num3/num1)#12
+print(num3 / num1)#13
+print(num2 / num1)#14
+print(num3 + num2 - num1)#15
+print(num3 * num2 / num1) #16
+print(num1 * num2 - num3) #17
+print(num1 * num2 / num3) #18
+print(num3 / num2 / num1)#19
+print(num3 * num2 * num1)#20
+
+
+#task 2) - შექმენით 2 string ტიპის ცვლადი და გატესტეთ როგორ მუშაობს კონკატენაცია.
+
+stringvar = "GOA"
+print(stringvar + " " + "is The best")
+
+#task  შექმენით 4 ცვლადი, სხვადასხვა ტიპების ( int, str, float, bool) და გატესტეთ როგორ მუშაობს ფუნქცია type()
+
+intvar = 10
+strvar = "string"
+floatvar = 10.2
+boolvar = True
+
+print(type(intvar))
+print(type(strvar))
+print(type(floatvar))
+print(type(boolvar))
---
+++ Level3/Homework/Lesson.py
@@ -0,0 +1,15 @@
+my = "my"
+name ="name"
+luka = "luka"
+age = 20 
+city ="tbilisi"
+male = True
+female = False 
+height = 1.76
+developer = True
+hobby = "Music"
+
+print(my  + " " +  name + " "+  'is'+" "  + luka + "," + "I live in " + city +  ","+ "Am i male? " + str(male) + "." + "Am i female? " + str(female) +"." + "my height is " + f"{height}" + "." + "Am i developer? " + str(developer) + "." + " My hobby is " + hobby  )
+
+
+
